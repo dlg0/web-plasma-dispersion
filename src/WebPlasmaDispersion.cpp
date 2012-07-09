b6660afaf174e35fccb9f299d6dcf6c0620ce195
dlg0 <dlg0@me.com>
2012-07-09 17:20:51 -0400

    Added first plot

--- src/WebPlasmaDispersion.cpp
+++ src/WebPlasmaDispersion.cpp
@@ -11,12 +11,19 @@
 #include <Wt/WGroupBox>
 #include <Wt/WButtonGroup>
 #include <Wt/WRadioButton>
+#include <sstream>
+#include <Wt/WBorderLayout>
+#include <Wt/WImage>
+#include <Wt/Chart/WCartesianChart>
+#include <Wt/WStandardItemModel>
 
-class IonSpecParameters
+class IonSpec
 {
 		public:
-		IonSpecParameters(void);
-		~IonSpecParameters(void);
+		IonSpec(int, Wt::WGroupBox*);
+		~IonSpec(void);
+		void setVisible(void);
+		void setHidden(void);
 
 		private:
 
@@ -24,15 +31,41 @@ class IonSpecParameters
 		Wt::WLineEdit *SpecAMU_LineEdit, *SpecZ_LineEdit;
 		Wt::WBreak *SpecBreak;
 		Wt::WText *SpecText;
+		int SpecNo;
 };
 
-IonSpecParameters::IonSpecParameters(void)
+IonSpec::IonSpec(int SpecNoIn, Wt::WGroupBox *IonSpecies_Container )
+{
+	SpecNo = SpecNoIn;
+	std::stringstream TmpStr;
+	TmpStr << "Ion Species " << SpecNoIn << " (AMU, Z)";
+	Wt::WString TmpStrWt(TmpStr.str());
+	SpecText = new Wt::WText(TmpStrWt,IonSpecies_Container);
+	SpecText->setHidden(1);
+	SpecAMU_LineEdit = new Wt::WLineEdit(IonSpecies_Container);
+	SpecAMU_LineEdit->setHidden(1);
+	SpecZ_LineEdit = new Wt::WLineEdit(IonSpecies_Container);
+	SpecZ_LineEdit->setHidden(1);
+	SpecBreak = new Wt::WBreak(IonSpecies_Container);
+	SpecBreak->setHidden(1);
+}
+IonSpec::~IonSpec(void)
 {
 }
-IonSpecParameters::~IonSpecParameters(void)
+void IonSpec::setVisible(void)
 {
+	SpecText->setHidden(0);
+	SpecAMU_LineEdit->setHidden(0);
+	SpecZ_LineEdit->setHidden(0);
+	SpecBreak->setHidden(0);
 }
-
+void IonSpec::setHidden(void)
+{
+	SpecText->setHidden(1);
+	SpecAMU_LineEdit->setHidden(1);
+	SpecZ_LineEdit->setHidden(1);
+	SpecBreak->setHidden(1);
+}
 
 class HelloApplication : public Wt::WApplication
 {
@@ -46,21 +79,34 @@ class HelloApplication : public Wt::WApplication
 			Wt::WDoubleValidator *DensityValidator, *BFieldValidator, *FreqValidator;
 			Wt::WText *DensityValidText, *BFieldValidText, *FreqValidText;
 
-			std::vector<IonSpecParameters> species;
+			Wt::WContainerWidget *w;
+			Wt::WBorderLayout *layout;
 
-			Wt::WLineEdit *Spec1AMU, *Spec2AMU, *Spec3AMU, *Spec4AMU, *Spec5AMU, *Spec6AMU;
-			Wt::WLineEdit *Spec1Z, *Spec2Z, *Spec3Z, *Spec4Z, *Spec5Z, *Spec6Z;
-			Wt::WText *Spec1Text, *Spec2Text, *Spec3Text, *Spec4Text, *Spec5Text, *Spec6Text;
-			Wt::WBreak *Spec1Break, *Spec2Break, *Spec3Break, *Spec4Break, *Spec5Break, *Spec6Break;
-			Wt::WGroupBox *HotOrCold_Container, *ParameterType_Container, *IonSpecies_Container, 
+			std::vector<IonSpec> species;
+
+			Wt::WGroupBox *West_Container, *ParameterType_Container, *IonSpecies_Container, 
 					*PointParameterType_Container,*MagneticParameterType_Container;
 			Wt::WButtonGroup *ParameterType_ButtonGroup;
 			Wt::WComboBox *nIonSpecies_ComboBox;
 
+			// Scan
+			int nX;
+			Wt::WText *nX_Text;
+			Wt::WLineEdit *nX_LineEdit;
+
+			// Magnetic field scan
+			float r0, b0, rMin, rMax;
+			Wt::WText *r0_Text, *b0_Text, *rMin_Text, *rMax_Text;
+			Wt::WLineEdit *r0_LineEdit, *b0_LineEdit, *rMin_LineEdit, *rMax_LineEdit;
+
+			Wt::Chart::WCartesianChart *b_CartesianChart;
+
 			void greet();
 			void editedLineEdit(Wt::WLineEdit *LineEditIn, Wt::WText *ValidText);
-			void showIonSpecies();
+			void AddIonSpecies();
 			void RevealParameterSettings();
+
+			int nSpecies;
 
 };
 
@@ -68,34 +114,37 @@ HelloApplication::HelloApplication(const Wt::WEnvironment& env) : Wt::WApplicati
 {
 		    setTitle("Plasma Dispersion Calculator");
 
-			//root()->addWidget(new Wt::WText("Your name, please ? "));
-			//nameEdit_ = new Wt::WLineEdit(root());
-			//Wt::WPushButton *button = new Wt::WPushButton("RUN", root());
-			//root()->addWidget(new Wt::WBreak());
-			//greeting_ = new Wt::WText(root());
-			//button->clicked().connect(this, &HelloApplication::greet);
+			w = new Wt::WContainerWidget(root());
+			layout = new Wt::WBorderLayout();
+			w->setLayout(layout,Wt::AlignTop | Wt::AlignJustify);
+
+			//layout->addWidget(new Wt::WImage("naruto.jpg"),Wt::WBorderLayout::Center);
 
 			enum CalculationType { Hot = 1, Cold = 2 };
 
-			HotOrCold_Container = new Wt::WGroupBox("Hot or Cold Calculation",root());
-			Wt::WButtonGroup *HotOrCold_ButtonGroup = new Wt::WButtonGroup(root());
-
+			West_Container = new Wt::WGroupBox("Parameters");
+			layout->addWidget(West_Container,Wt::WBorderLayout::West);
+			Wt::WButtonGroup *HotOrCold_ButtonGroup = new Wt::WButtonGroup(West_Container);
 			Wt::WRadioButton *HotOrCold_Button;
 
-			HotOrCold_Button = new Wt::WRadioButton("Hot", HotOrCold_Container);
-			new Wt::WBreak(HotOrCold_Container);
+			HotOrCold_Button = new Wt::WRadioButton("Hot", West_Container);
+			new Wt::WBreak(West_Container);
 			HotOrCold_ButtonGroup->addButton(HotOrCold_Button,Hot);
 
-			HotOrCold_Button = new Wt::WRadioButton("Cold", HotOrCold_Container);
-			new Wt::WBreak(HotOrCold_Container);
+			HotOrCold_Button = new Wt::WRadioButton("Cold", West_Container);
+			new Wt::WBreak(West_Container);
 			HotOrCold_ButtonGroup->addButton(HotOrCold_Button,Cold);
 
 			HotOrCold_ButtonGroup->setCheckedButton(HotOrCold_ButtonGroup->button(Cold));
 
+			nX_Text = new Wt::WText("nX",West_Container);
+			nX_LineEdit = new Wt::WLineEdit("100",West_Container);
+
 			enum ParameterType { Point = 1, MagneticField = 2, Numerical = 3 };
 
-			ParameterType_Container = new Wt::WGroupBox("Parameter Scan Type",root());
-			ParameterType_ButtonGroup = new Wt::WButtonGroup(root());
+			ParameterType_Container = new Wt::WGroupBox("Parameter Scan Type");
+			layout->addWidget(ParameterType_Container,Wt::WBorderLayout::East);
+			ParameterType_ButtonGroup = new Wt::WButtonGroup(ParameterType_Container);
 
 			Wt::WRadioButton *ParameterType_Button;
 
@@ -156,6 +205,20 @@ HelloApplication::HelloApplication(const Wt::WEnvironment& env) : Wt::WApplicati
 
 			MagneticParameterType_Container = new Wt::WGroupBox("Magnetic Field Scan Settings",root());
 			MagneticParameterType_Container->setHidden(0);
+			r0_Text = new Wt::WText("r0",MagneticParameterType_Container);
+			r0_LineEdit = new Wt::WLineEdit("2",MagneticParameterType_Container);
+			new Wt::WBreak(MagneticParameterType_Container);
+
+			b0_Text = new Wt::WText("b0",MagneticParameterType_Container);
+			b0_LineEdit = new Wt::WLineEdit("5",MagneticParameterType_Container);
+			new Wt::WBreak(MagneticParameterType_Container);
+
+			rMin_Text = new Wt::WText("rMin",MagneticParameterType_Container);
+			rMin_LineEdit = new Wt::WLineEdit("0.1",MagneticParameterType_Container);
+			new Wt::WBreak(MagneticParameterType_Container);
+
+			rMax_Text = new Wt::WText("rMax",MagneticParameterType_Container);
+			rMax_LineEdit = new Wt::WLineEdit("4",MagneticParameterType_Container);
 
 			IonSpecies_Container = new Wt::WGroupBox("Ion Species",root());
 
@@ -169,59 +232,39 @@ HelloApplication::HelloApplication(const Wt::WEnvironment& env) : Wt::WApplicati
 			nIonSpecies_ComboBox->addItem("6");
 			nIonSpecies_ComboBox->setCurrentIndex(0);
 			nIonSpecies_ComboBox->changed().connect(
-				boost::bind(&HelloApplication::showIonSpecies,this));
+				boost::bind(&HelloApplication::AddIonSpecies,this));
 
 			new Wt::WBreak(IonSpecies_Container);
 
-			Spec1Text = new Wt::WText("Ion Species 1 (AMU, Z)",IonSpecies_Container);
-			Spec1AMU = new Wt::WLineEdit("2",IonSpecies_Container);
-			Spec1Z = new Wt::WLineEdit("1",IonSpecies_Container);
-			Spec1Break = new Wt::WBreak(IonSpecies_Container);
+			nSpecies = 0;
+			HelloApplication::AddIonSpecies();
 
-			Spec2Text = new Wt::WText("Ion Species 2 (AMU, Z)",IonSpecies_Container);
-			Spec2Text->setHidden(1);
-			Spec2AMU = new Wt::WLineEdit(IonSpecies_Container);
-			Spec2AMU->setHidden(1);
-			Spec2Z = new Wt::WLineEdit(IonSpecies_Container);
-			Spec2Z->setHidden(1);
-			Spec2Break = new Wt::WBreak(IonSpecies_Container);
-			Spec2Break->setHidden(1);
+			b_CartesianChart = new Wt::Chart::WCartesianChart();
+			Wt::WStandardItemModel *model = new Wt::WStandardItemModel(40,2);
+	  		model->setHeaderData(0, Wt::WString("X"));
+	    	model->setHeaderData(1, Wt::WString("Y = sin(X)"));
 
-			Spec3Text = new Wt::WText("Ion Species 3 (AMU, Z)",IonSpecies_Container);
-			Spec3Text->setHidden(1);
-			Spec3AMU = new Wt::WLineEdit(IonSpecies_Container);
-			Spec3AMU->setHidden(1);
-			Spec3Z = new Wt::WLineEdit(IonSpecies_Container);
-			Spec3Z->setHidden(1);
-			Spec3Break = new Wt::WBreak(IonSpecies_Container);
-			Spec3Break->setHidden(1);
+		  	for (unsigned i = 0; i < 40; ++i) {
 
-			Spec4Text = new Wt::WText("Ion Species 4 (AMU, Z)",IonSpecies_Container);
-			Spec4Text->setHidden(1);
-			Spec4AMU = new Wt::WLineEdit(IonSpecies_Container);
-			Spec4AMU->setHidden(1);
-			Spec4Z = new Wt::WLineEdit(IonSpecies_Container);
-			Spec4Z->setHidden(1);
-			Spec4Break = new Wt::WBreak(IonSpecies_Container);
-			Spec4Break->setHidden(1);
+		      double x = (static_cast<double>(i) - 20) / 4;
+		      model->setData(i, 0, x);
+		      model->setData(i, 1, sin(x));
 
-			Spec5Text = new Wt::WText("Ion Species 5 (AMU, Z)",IonSpecies_Container);
-			Spec5Text->setHidden(1);
-			Spec5AMU = new Wt::WLineEdit(IonSpecies_Container);
-			Spec5AMU->setHidden(1);
-			Spec5Z = new Wt::WLineEdit(IonSpecies_Container);
-			Spec5Z->setHidden(1);
-			Spec5Break = new Wt::WBreak(IonSpecies_Container);
-			Spec5Break->setHidden(1);
+		    }
 
-			Spec6Text = new Wt::WText("Ion Species 6 (AMU, Z)",IonSpecies_Container);
-			Spec6Text->setHidden(1);
-			Spec6AMU = new Wt::WLineEdit(IonSpecies_Container);
-			Spec6AMU->setHidden(1);
-			Spec6Z = new Wt::WLineEdit(IonSpecies_Container);
-			Spec6Z->setHidden(1);
-			Spec6Break = new Wt::WBreak(IonSpecies_Container);
-			Spec6Break->setHidden(1);
+			b_CartesianChart->setModel(model);
+			b_CartesianChart->setType(Wt::Chart::ScatterPlot);
+			b_CartesianChart->setXSeriesColumn(0);
+			b_CartesianChart->setLegendEnabled(true);
+			b_CartesianChart->axis(Wt::Chart::XAxis).setLocation(Wt::Chart::ZeroValue);
+			b_CartesianChart->resize(800,300);
+
+
+			Wt::Chart::WDataSeries s(1,Wt::Chart::CurveSeries);
+			b_CartesianChart->addSeries(s);
+
+			layout->addWidget(b_CartesianChart,Wt::WBorderLayout::Center);
+
 
 }
 
@@ -243,105 +286,28 @@ void HelloApplication::RevealParameterSettings()
 		}
 }
 
-void HelloApplication::showIonSpecies()
+void HelloApplication::AddIonSpecies()
 {
 
-		int nSpec = nIonSpecies_ComboBox->currentIndex();
-		std::cout << nSpec << std::endl;
-		nSpec++;
+		int nSpecTmp = nIonSpecies_ComboBox->currentIndex();
+		std::cout << nSpecTmp << std::endl;
 
-		IonSpecParameters mySpecParams;	
-		for(int s=0; s<nSpec; s++)
+		for(int s=0; s<nSpecies; s++)
 		{
-				//Fill in mySpecParams then add to the species vec.
-				// ....
+				species[s].setHidden();
+		}
+
+		species.clear();
+		nSpecies = 0;
+
+		for(int s=0; s<=nSpecTmp; s++)
+		{
+				IonSpec mySpecParams(s+1,IonSpecies_Container);	
 				species.push_back(mySpecParams);
+				species[s].setVisible();
+				nSpecies++;
 		}
 
-		if(nSpec>=1){
-			Spec1Text->setHidden(0);
-			Spec1AMU->setHidden(0);
-			Spec1Z->setHidden(0);
-			Spec1Break->setHidden(0);
-		}
-		else
-		{
-			Spec1Text->setHidden(1);
-			Spec1AMU->setHidden(1);
-			Spec1Z->setHidden(1);
-			Spec1Break->setHidden(1);
-		}
-
-		if(nSpec>=2){
-			Spec2Text->setHidden(0);
-			Spec2AMU->setHidden(0);
-			Spec2Z->setHidden(0);
-			Spec2Break->setHidden(0);
-		}
-		else
-		{
-			Spec2Text->setHidden(1);
-			Spec2AMU->setHidden(1);
-			Spec2Z->setHidden(1);
-			Spec2Break->setHidden(1);
-		}
-	
-		if(nSpec>=3){
-			Spec3Text->setHidden(0);
-			Spec3AMU->setHidden(0);
-			Spec3Z->setHidden(0);
-			Spec3Break->setHidden(0);
-		}
-		else
-		{
-			Spec3Text->setHidden(1);
-			Spec3AMU->setHidden(1);
-			Spec3Z->setHidden(1);
-			Spec3Break->setHidden(1);
-		}
-	
-		if(nSpec>=4){
-			Spec4Text->setHidden(0);
-			Spec4AMU->setHidden(0);
-			Spec4Z->setHidden(0);
-			Spec4Break->setHidden(0);
-		}
-		else
-		{
-			Spec4Text->setHidden(1);
-			Spec4AMU->setHidden(1);
-			Spec4Z->setHidden(1);
-			Spec4Break->setHidden(1);
-		}
-		
-		if(nSpec>=5){
-			Spec5Text->setHidden(0);
-			Spec5AMU->setHidden(0);
-			Spec5Z->setHidden(0);
-			Spec5Break->setHidden(0);
-		}
-		else
-		{
-			Spec5Text->setHidden(1);
-			Spec5AMU->setHidden(1);
-			Spec5Z->setHidden(1);
-			Spec5Break->setHidden(1);
-		}
-
-		if(nSpec>=6){
-			Spec6Text->setHidden(0);
-			Spec6AMU->setHidden(0);
-			Spec6Z->setHidden(0);
-			Spec6Break->setHidden(0);
-		}
-		else
-		{
-			Spec6Text->setHidden(1);
-			Spec6AMU->setHidden(1);
-			Spec6Z->setHidden(1);
-			Spec6Break->setHidden(1);
-		}
-	
 }
 
 void HelloApplication::editedLineEdit(Wt::WLineEdit *LineEditIn, Wt::WText *ValidText)
